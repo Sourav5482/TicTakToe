3c2df2cc1bc0e8adce243418ba3ab9d2eeec3a9c
SOURAV DAS <bcasourav5482@gmail.com>
2024-10-28 14:27:09 +0530

    Add files via upload

---
+++ quete.js
@@ -0,0 +1,21 @@
+export const quete = [` “Winning means being unafraid to lose.”
+- Fran Tarkenton`, `“Sooner or later, those who win are those who think they can.”
+- Paul Turnier`, `“With ordinary talent and extraordinary perseverance, all things are attainable.”
+- Sir Thomas Buxton`, `“Winning doesn’t always mean being first. Winning means you’re doing better than you’ve ever done before.”
+- Bonnie Blair`, ` “A positive attitude gives you power over your circumstances instead of your circumstances having power over you.”
+– Joyce Meyer`, `“If you have the will to win, you have achieved half your success; if you don’t, you have achieved half your failure.”
+- David Ambrose`,`“You were born to win, but to be a winner, you must plan to win, prepare to win, and expect to win.”
+– Zig Ziglar`
+
+
+];
+
+// if(userX){
+//     box.textContent="X"
+     
+//  }else{
+//      let a=["X","O"]
+//     let j= Math.floor(Math.random()*2)
+//     console.log(j);
+//      box.textContent=a[j] 
+//  }
